dac8f889785901ecf18108f5f17f19a928c22316
unknown <C:\The Bat! Pwd>
2017-11-11 00:07:05 +0530

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.HEROKU_POSTGRESQL_GOLD_URL ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
