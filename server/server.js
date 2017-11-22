9971dbb74b2e7233d38bec4ceabf5d8344ce5381
unknown <C:\The Bat! Pwd>
2017-11-22 23:07:54 +0530

    Added private route

--- server/server.js
+++ server/server.js
@@ -13,9 +13,11 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos',authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator:req.user._id
+
   });
 
   todo.save().then((doc) => {
@@ -25,23 +27,24 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todo', (req, res)=> {
-  Todo.find().then((todo)=> {
+app.get('/todo', authenticate , (req, res)=> {
+  Todo.find({_creator:req.user._id}).then((todo)=> {
     res.send({todo});
   },(e)=>{
     res.status(400).send(e)
   })
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate, (req,res)=>{
 var id = req.params.id;
+
 if(!(ObjectID.isValid(id))){
   return res.status(400).send('Invalid Id');
 }
-// else{
-//   return res.status(200).send();
-// }
-Todo.findById(id).then((doc)=>{
+Todo.findOne({
+  _id:id,
+  _creator:req.user._id
+}).then((doc)=>{
   if(!doc){
     return res.status(404).send();
 }
@@ -55,7 +58,10 @@ app.delete('/todos/:id', (req, res)=>{
   if(!ObjectID.isValid(id)){
     return res.status(400).send('Invalid id, please check the Id');
   }
-  Todo.findByIdAndRemove('id').then((doc)=>{
+  Todo.findOneAndRemove({
+    _id :id,
+    _creator : req.user._id
+  }).then((doc)=>{
     if(!doc){
       return res.status(404).send();
     }
@@ -65,7 +71,7 @@ app.delete('/todos/:id', (req, res)=>{
   });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate , (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -80,7 +86,12 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findOneAndUpdate({
+  _id:id,
+  _creator: req.user._id
+  },
+   {$set: body},
+    {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
